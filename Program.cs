49113680ddb2720818e844ca86be8b590f20d8ab
f0xy3u <skopec2008@gmail.com>
2024-12-10 11:42:18 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace rpgSurvival
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Vánoční RPG!");
+        }
+    }
+}
